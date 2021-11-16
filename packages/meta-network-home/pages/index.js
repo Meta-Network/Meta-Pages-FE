22f04eabaa796418a63b174b744ec69b4119fe36
Casimir Crystal <casimir.crystal.blue@outlook.com>
2021-11-16 23:33:47 +0800

    fix: mobile phone adaptation

--- packages/meta-network-home/pages/index.js
+++ packages/meta-network-home/pages/index.js
@@ -3,31 +3,34 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Footer from '../components/Footer';
+import ButtonToIndex from '../components/ButtonToIndex';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Meta Network</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preload" href="/images/background.png" as="image" />
+
+        <link rel="preload" href="http://127.0.0.1:3000/" as="image" />
         <link rel="preload" href="/images/card.png" as="image" />
       </Head>
 
       <main>
-        <card style={{marginLeft: '10vw'}}>
+        <card className="first-card">
           <h1 className="title">
             <img
               className="logo-before-title"
               src="/images/logos/meta-network.png"
-             alt="Meta Network Logo" />
+              alt="Meta Network Logo"
+            />
             <span className="colored-title">META</span> NETWORK
           </h1>
 
           <div className="description">
             <p>
               <span>
-                2021·11·22 ｜19:00～21:00 (GMT+8)
+                2021·11·22 ｜19:00～21:30 (GMT+8)
               </span>
             </p>
             <p>
@@ -36,52 +39,45 @@ export default function Home() {
               }}>
                 正式发布
               </span>
-              <a href="https://www.metanetwork.online/" target="__blank">
-                <button className="to-meta-network">
-                  抢先占领
-                </button>
-              </a>
+              <ButtonToIndex />
             </p>
-            <p style={{ marginTop: 20 }}>
+            <p className="description-extra">
               <a href="https://www.matataki.io/" target="__blank" style={{ paddingRight: 15 }}>
                 <img className="logo-link" src="/images/logos/matataki.png"  alt="Matataki"/>
               </a>
               <a href="https://www.meta.io/" target="__blank">
                 <img className="logo-link" src="/images/logos/meta-io.png"  alt="Meta-io"/>
               </a>
-
             </p>
           </div>
         </card>
 
-        <card className="rightCard">
-          <Image
-            priority
-            width={850}
-            height={470}
-            src="/images/card.png"
-          />
+        <card className="second-card">
+          <div className="card-image-container">
+            <img
+              className="card-image"
+              src="/images/card.png"
+            />
+          </div>
         </card>
+
+        <Footer />
       </main>
 
-      <Footer />
 
-      <style jsx>{`
+      <style jsx preload>{`
         @font-face {
-          font-family: Oriya MN; src: url('oriya-mn.ttf');
+          font-family: Oriya MN;
+          src: url('oriya-mn.ttf');
         }
-        .container {
-          color: white;
-          min-height: 100vh;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-direction: row;
-        }
-        
+
         main {
           display: flex;
           justify-content: space-around;
+          color: white;
+          min-height: 100vh;
+          align-items: center;
+          flex-direction: row;
         }
 
         card {
@@ -90,12 +86,12 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
         }
-        
+
         .title {
           font-family: Oriya MN, sans-serif;
           font-size: 3.3vw;
         }
-        
+
         span.colored-title {
           color: rgb(218, 254, 96);
         }
@@ -108,64 +104,102 @@ export default function Home() {
           position: relative;
           margin-right: -3vw;
         }
-        
-        .rightCard {
+
+        .first-card {
+          margin-left: 10vw;
+        }
+
+        .second-card {
           margin-right: 10vw;
           position: relative;
           display: inline-block;
         }
 
+        .card-image {
+          width: 40vw;
+          height: 22vw;
+        }
+
+        .card-image-container {
+          box-sizing: border-box;
+          display: inline-block;
+          overflow: hidden;
+          width: initial;
+          height: initial;
+          border: 0;
+          margin: 0;
+          padding: 0;
+          position: relative;
+          max-width: 100%;
+        }
+
         .description {
           display: flex;
+          white-space: nowrap;
           flex-direction: column;
           font-family: Source Han Sans, Droid Sans, Helvetica Neue, sans-serif;
           font-size: 1.6vw;
           line-height: 2;
           justify-content: center;
         }
-        
+
         .logo-link {
           width: 3vw;
           height: 3vw;
         }
-        
-        .to-meta-network {
-          margin-left: 2vw;
-          vertical-align: middle;
-          min-width: 9vw;
-          min-height: 2.8vw;
-          font-size: 1vw;
-          color: black;
-          padding: 5px 10px;
-          font-weight: bold;
-          cursor: pointer;
-          transition: all 0.3s ease;
-          position: relative;
-          display: inline-block;
-          outline: none;
-          overflow: hidden;
-          border-radius: 5px;
-          border: none;
-          background-color: rgb(218, 254, 96);
-        }
-        .to-meta-network:hover {
-          border-radius: 5px;
-          padding-right: 24px;
-          padding-left:8px;
-        }
-        .to-meta-network:hover:after {
-          opacity: 1;
-          right: 10px;
-        }
-        .to-meta-network:after {
-          content: '»';
-          position: absolute;
-          opacity: 0;
-          font-size: 20px;
-          line-height: 40px;
-          top: 0;
-          right: -20px;
-          transition: 0.4s;
+
+        @media only screen and (max-width: 600px) {
+          main {
+            flex-direction: column;
+            justify-content: space-between;
+          }
+
+          .title {
+            font-size: 9vw;
+          }
+
+          .description {
+            font-size: 4.5vw;
+          }
+
+          .first-card {
+            margin-left: 10vw;
+            margin-top: 10vw;
+          }
+
+          .second-card {
+            display: flex;
+            margin: 50px 0 0;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .card-image {
+            width: 80vw;
+            height: 44vw;
+          }
+
+          .logo-before-title {
+            width: 8vw;
+            height: 8.2vw;
+            top: 1vw;
+            right: 10vw;
+            position: relative;
+            margin-right: -8vw;
+          }
+
+          p {
+            margin: 1vw 0 !important;
+          }
+
+          .logo-link {
+            width: 7vw;
+            height: 7vw;
+          }
+
+          .description-extra {
+            margin-top: 2vw !important;
+          }
         }
       `}</style>
 
@@ -180,7 +214,7 @@ export default function Home() {
         
         p {
           padding: 0;
-          margin: 8px 0;
+          margin: 0.5vw 0;
         }
       `}</style>
     </div>
